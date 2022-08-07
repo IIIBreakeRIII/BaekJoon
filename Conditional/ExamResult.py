8d61fe6bd9572f86b9ddd7735e1dbcbd842ddb01
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-08-07 12:00:00 +0900

    No. 2753

--- Conditional/ExamResult.py
+++ Conditional/ExamResult.py
@@ -1,12 +1,12 @@
 a = int(input())
 
-if a > 90:
+if a >= 90:
     print("A")
-elif 89 >= a > 80:
+elif 90 > a >= 80:
     print("B")
-elif 79 >= a > 70:
+elif 80 > a >= 70:
     print("C")
-elif 69 >= a > 60:
+elif 70 > a >= 60:
     print("D")
 else:
     print("F")
