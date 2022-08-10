27bdf23f6d00a06a3d316d711b5e2adc3fa48aad
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-08-10 16:43:45 +0900

    No. 2884

---
+++ Conditional/Alarm.py
@@ -0,0 +1,16 @@
+H, M = input().split()
+H = int(H)
+M = int(M)
+
+if M >= 45:
+    M = M - 45
+    print(H, M)
+else:
+    if H == 0:
+        H = 23
+        M = M + 15
+        print(H, M)
+    else:
+        H = H - 1
+        M = M + 15
+        print(H, M)
