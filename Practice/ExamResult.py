7ea28c674281ddc671d8030ebdab17ddfcd7c8e6
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-09-09 21:42:31 +0900

    No. 2754

---
+++ Practice/ExamResult.py
@@ -0,0 +1,28 @@
+result = input()
+
+if result == "A+":
+  print("4.3")
+elif result == "A0":
+  print("4.0")
+elif result == "A-":
+  print("3.7")
+elif result == "B+":
+  print("3.3")
+elif result == "B0":
+  print("3.0")
+elif result == "B-":
+  print("2.7")
+elif result == "C+":
+  print("2.3")
+elif result == "C0":
+  print("2.0")
+elif result == "C-":
+  print("1.7")
+elif result == "D+":
+  print("1.3")
+elif result == "D0":
+  print("1.0")
+elif result == "D-":
+  print("0.7")
+elif result == "F":
+  print("0.0")
