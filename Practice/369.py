a6c4e6e157f783a3bc7e71a249f09bd5fdff63a2
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-11-01 10:09:42 +0900

    3, 6, 9 Game

---
+++ Practice/369.py
@@ -0,0 +1,19 @@
+from ast import Num
+
+
+A = int(input())
+count = 0
+
+for i in range(A + 1):
+  NumberList = list(map(int, str(i)))
+  for j in range(len(NumberList)):
+    if NumberList[j] == 3:
+      count += 1
+    elif NumberList[j] == 6:
+      count += 1
+    elif NumberList[j] == 9:
+      count += 1
+    else:
+      continue
+
+print(count)
