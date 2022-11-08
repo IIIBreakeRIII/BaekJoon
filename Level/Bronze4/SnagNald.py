118f57ee7ac0720a69df3bd4002d9b25638b259c
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-11-08 10:12:49 +0900

    No. 5543

---
+++ Level/Bronze4/SnagNald.py
@@ -0,0 +1,10 @@
+Burger1 = int(input())
+Burger2 = int(input())
+Burger3 = int(input())
+Coke = int(input())
+Cider = int(input())
+
+BugerList = [Burger1, Burger2, Burger3]
+DrinkList = [Coke, Cider]
+
+print(min(BugerList) + min(DrinkList) - 50)
