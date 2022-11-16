bcbaba5b618496edcdf3f07226252ce74b1ea567
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-11-16 17:07:50 +0900

    No. 10808

---
+++ Level/Bronze4/CountAlphabet.py
@@ -0,0 +1,7 @@
+InputString = list(str(input()))
+
+Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in range(len(Alphabet)):
+  Count = InputString.count(Alphabet[i])
+  print(Count, end=' ')
