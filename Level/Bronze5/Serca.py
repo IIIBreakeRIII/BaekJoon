7b1af3ae0133df1df9cdbf8afe7c59c1612da522
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-01-23 12:00:00 +0900

    No. 26766

---
+++ Level/Bronze5/Serca.py
@@ -0,0 +1,17 @@
+# No. 26766 Serca
+
+def serca():
+  print(" @@@   @@@")
+  print("@   @ @   @")
+  print("@    @    @")
+  print("@         @")
+  print(" @       @")
+  print("  @     @")
+  print("   @   @")
+  print("    @ @")
+  print("     @")
+
+Value = int(input())
+
+for i in range(Value):
+  serca()
