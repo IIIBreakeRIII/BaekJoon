442c4682f248d71d0fe6e29a8f8fb6afaaf2b8f5
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-02-07 19:52:22 +0900

    11653 시간 초과

--- Basic_Math/Factorization.py
+++ Basic_Math/Factorization.py
@@ -1,20 +1,47 @@
 # No. 11653 소인수분해
+# 기존 코드 시간 초과
+
+# 다음은 시간 초과 코드
+
+# N = int(input())
+# FactorizeNum = 2
+# 
+# while(True):
+#     if N % FactorizeNum == 0:
+#         print(FactorizeNum)
+#         N = N // FactorizeNum
+#     else:
+#         if FactorizeNum > N:
+#             break
+#         else:
+#             FactorizeNum = FactorizeNum + 1
+#             for i in range(FactorizeNum):
+#                 if i == 0:
+#                     continue
+#                 else:
+#                     if FactorizeNum % i == 0:
+#                         break
+
+import time
 
 N = int(input())
 FactorizeNum = 2
+
+start = time.time()
 
 while(True):
     if N % FactorizeNum == 0:
         print(FactorizeNum)
         N = N // FactorizeNum
-    elif N % FactorizeNum != 0:
+    else:
         if FactorizeNum > N:
             break
         else:
             FactorizeNum = FactorizeNum + 1
-            for i in range(FactorizeNum):
-                if i == 0:
-                    continue
-                else:
-                    if FactorizeNum % i == 0:
+            for i in range(2, FactorizeNum):
+                if FactorizeNum % i == 0:
                         break
+
+end = time.time()
+
+print(f"{end - start:.5f} seconds")
