ee3b0a6a837c37c02992ef6c2c01e54897c95927
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-02-23 17:35:00 +0900

    No. 2444

---
+++ Loop/PrintStar3.py
@@ -0,0 +1,14 @@
+num = int(input())
+temp = 1
+
+for i in range(1, num * 2):
+    if i == num * 2:
+        break
+    elif i == num:
+        print("*" * (num * 2 - 1))
+    elif i < num:
+        print(" " * (num - i) + "*" * (i * 2 - 1))
+    elif i > num:
+        index = i - (2 * temp)
+        print(" " * (i - num) + "*" * (index * 2 - 1))
+        temp = temp + 1
