beb94b240545b692d9b1c3a9076c433431b97ddd
IIIBreakeRIII <ryu990305@icloud.com>
2023-05-19 12:00:00 +0900

    No. 2577

---
+++ Level/Bronze2/CountNum.py
@@ -0,0 +1,16 @@
+# No. 2577 숫자의 개수
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = A * B * C
+list_result = list(map(int, str(result)))
+
+count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(len(list_result)):
+    count[list_result[i]] = count[list_result[i]] + 1
+
+for j in range(len(count)):
+    print(count[j])
