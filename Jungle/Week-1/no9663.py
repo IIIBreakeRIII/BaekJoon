02b26b76cb0970737d922efcbfec6c834a2de534
IIIBreakeRIII <ryu990305@icloud.com>
2025-07-15 01:58:06 +0900

    No.9663 N-Queen 재풀이 필요

---
+++ Jungle/Week-1/no9663.py
@@ -0,0 +1,26 @@
+n = int(input())
+ 
+def attack(x):
+    for i in range(x): 
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return True
+
+    return False
+ 
+def dfs(start):
+    global cnt
+
+    if start == n: 
+        cnt += 1
+    else:
+        for i in range(n):
+            row[start] = i
+            if not attack(start):
+                print(start + 1)
+                dfs(start + 1)
+ 
+row = [0] * n
+cnt = 0
+dfs(0)
+ 
+print(cnt)
