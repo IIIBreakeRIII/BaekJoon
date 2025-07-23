28ed5b322f574bd9f6e32c9d7ce1fa49bee38d59
IIIBreakeRIII <ryu990305@icloud.com>
2025-07-23 17:44:18 +0900

    No.2812 크게 만들기

---
+++ Jungle/Week-2/no2812.py
@@ -0,0 +1,22 @@
+# No.2812 크게 만들기
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+numbers = input().rstrip()
+
+stack = []
+
+for num in numbers:
+    # 스택이 비어있거나, 스택에 현재 마지막에 있는 원소가
+    # number보다 작으면 pop, k 즉 앞에서부터 제거하는 경우를 1개 줄여줌
+    while stack and stack[-1] < num and k > 0:
+        stack.pop()
+        k -= 1
+    stack.append(num)
+
+if k > 0:
+    print(''.join(stack[:-k]))
+else:
+    print(''.join(stack))
