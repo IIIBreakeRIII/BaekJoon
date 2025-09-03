c291f1a5c07c9307a81cdd0596674ed1a0ae0302
IIIBreakeRIII <ryu990305@icloud.com>
2025-09-03 20:32:50 +0900

    [Solved] No.2630 색종이 만들기

---
+++ Jungle/week-8/no2630.py
@@ -0,0 +1,74 @@
+# No.2630 색종이 만들기
+
+import sys
+
+input = sys.stdin.readline
+paper = []
+result = []
+
+def solution(x, y, n):
+
+    # current location color = 1 or 0
+    color = paper[x][y]
+    
+    # col : start from current-location to + last location
+    for i in range(x, x + n):
+        # row : start from current-location to + last location
+        for j in range(y, y + n):
+            # if different with current color
+            if color != paper[i][j]:
+                # divide by 4
+                solution(x, y, n // 2)
+                solution(x, y + n // 2, n // 2)
+                solution(x + n // 2, y, n //2)
+                solution(x + n // 2, y + n // 2, n // 2)
+                
+                return
+
+    if color == 0:
+        result.append(0)
+    else:
+        result.append(1)
+
+n = int(input())
+for _ in range(n):
+    paper.append(list(map(int, input().split())))
+
+solution(0, 0, n)
+print(result.count(0))
+print(result.count(1))
+
+# import sys
+#
+# input = sys.stdin.readline
+# paper = []
+# result = []
+#
+# n = int(input())
+#
+# for i in range(n):
+#     paper.append(list(map(int, input().split())))
+#
+# def solution(x, y, n):
+#   color = paper[x][y]
+#
+#   for i in range(x, x + n):
+#     for j in range(y, y + n):
+#       if color != paper[i][j]:
+#         solution(x, y, n // 2)
+#         solution(x, y + n // 2, n // 2)
+#         solution(x + n // 2, y, n // 2)
+#         solution(x + n // 2, y + n // 2, n // 2)
+#
+#         return
+#
+#   if color == 0:
+#     result.append(0)
+#
+#   else:
+#     result.append(1)
+#
+#
+# solution(0, 0, n)
+# print(result.count(0))
+# print(result.count(1))
